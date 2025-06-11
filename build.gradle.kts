6bc235723cf3d358ada3cb463275915e1219f098
David Lopez Dayer <dayerdl@gmail.com>
2025-06-11 14:45:23 +0200

    ktor code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
-
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
     application
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.example.userservice.ApplicationKt")
 }
 
 repositories {
@@ -9,14 +16,11 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.0")
-    implementation("io.ktor:ktor-server-netty:2.3.0")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
-    testImplementation("io.ktor:ktor-server-tests:2.3.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.10")
-}
-
-application {
-    mainClass.set("com.example.ApplicationKt")
+    implementation("io.ktor:ktor-server-core-jvm:2.3.2")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.2")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
 }
--- settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-
-rootProject.name = "railway-kotlin-api"
