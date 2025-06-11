9ee404a18d1762f8aeb30097ee4fb69e99df382d
David Lopez Dayer <dayerdl@gmail.com>
2025-06-11 16:53:29 +0200

    kdein sample

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
-    application
+    kotlin("jvm") version "2.1.20"
+    id("io.ktor.plugin") version "3.1.3"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
-group = "com.example"
-version = "0.0.1"
-
 application {
-    mainClass.set("com.example.userservice.ApplicationKt")
+    mainClass.set("io.ktor.samples.kodein.KodeinSimpleApplicationKt")
 }
 
 repositories {
@@ -16,11 +16,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:2.3.2")
-    implementation("io.ktor:ktor-server-netty-jvm:2.3.2")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.2")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-resources")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-html-builder")
+    implementation("org.kodein.di:kodein-di-jvm:7.17.0")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
